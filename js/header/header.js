a15696221c952f397f19e1eef14fbf838a09ec70
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-01-20 20:47:17 +0100

    new header for home

--- js/header/header.js
+++ js/header/header.js
@@ -1,11 +1,6 @@
 define(['foliage', 'blossom!./header.less'],
        function(f) {
-	   console.log("define element");
 	   var header = f.element('header');
-	   
 	   return header(
-	       f.div({'class': 'container'},
-			 f.div({'class': 'row'},
-			       f.div({'class': 'span8'}, 
-				     f.img({src: 'images/logo.svg'})))));
+	       f.div({'class': 'container home'}));
 });
